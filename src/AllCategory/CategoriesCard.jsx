258164065bfd83a5d563737e84638795f52fd52d
Fatema Khanom <fatemaurmi2019@gmail.com>
2023-09-27 18:41:43 +0600

    fixed some error

--- src/AllCategory/CategoriesCard.jsx
+++ src/AllCategory/CategoriesCard.jsx
@@ -1,68 +1,8 @@
 
 
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-// const CategoriesCard = ({ card }) => {
-//   const {
-//     id,
-//     Picture,
-//     Category,
-//     Title,
-//     Category_bg_Color,
-//     Card_bg_Color,
-//     Text_button_bg_Color,
-//   } = card || {};
-
-  
-//   const cardStyle = {
-//     backgroundColor: Card_bg_Color,
-    
-//   };
-
-//   const categoryStyle = {
-//     backgroundColor: Category_bg_Color,
-//     color: Text_button_bg_Color,
-     
-    
-
-//   };
-//   const textStyle ={
-//     color: Text_button_bg_Color ,
-//   }
-
-//   return (
-//     <div>
-//       <Link to={`/donation-details/${id}`}>
-//       <div
-//         className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 h-72"
-//         style={cardStyle} 
-//       >
-//         <div
-//           className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none"
-          
-//         >
-//           <img className="h-[250px]" src={Picture} alt="" />
-//         </div>
-//         <div className="p-6">
-//           <h4 className="font-semibold inline-flex px-2 "style={categoryStyle} >
-//             {Category}
-//           </h4>
-//           <p className="mt-3 block font-sans font-bold leading-relaxed text-gray-700 antialiased " style={textStyle}>
-//             {Title}
-//           </p>
-//         </div>
-//       </div>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default CategoriesCard;
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const CategoriesCard = ({ card, onClick }) => {
+const CategoriesCard = ({ card }) => {
   const {
     id,
     Picture,
@@ -73,43 +13,49 @@ const CategoriesCard = ({ card, onClick }) => {
     Text_button_bg_Color,
   } = card || {};
 
+  
   const cardStyle = {
     backgroundColor: Card_bg_Color,
+    
   };
 
   const categoryStyle = {
     backgroundColor: Category_bg_Color,
     color: Text_button_bg_Color,
-  };
+     
+    
 
-  const textStyle = {
-    color: Text_button_bg_Color,
   };
+  const textStyle ={
+    color: Text_button_bg_Color ,
+  }
 
   return (
     <div>
-      <Link to={`/donation-details/${id}`} onClick={() => onClick(id)}>
+      <Link to={`/donation-details/${id}`}>
+      <div
+        className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 h-72"
+        style={cardStyle} 
+      >
         <div
-          className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 h-72"
-          style={cardStyle}
+          className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none"
+          
         >
-          <div
-            className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none"
-          >
-            <img className="h-[250px]" src={Picture} alt="" />
-          </div>
-          <div className="p-6">
-            <h4 className="font-semibold inline-flex px-2" style={categoryStyle}>
-              {Category}
-            </h4>
-            <p className="mt-3 block font-sans font-bold leading-relaxed text-gray-700 antialiased" style={textStyle}>
-              {Title}
-            </p>
-          </div>
+          <img className="h-[250px]" src={Picture} alt="" />
         </div>
+        <div className="p-6">
+          <h4 className="font-semibold inline-flex px-2 "style={categoryStyle} >
+            {Category}
+          </h4>
+          <p className="mt-3 block font-sans font-bold leading-relaxed text-gray-700 antialiased " style={textStyle}>
+            {Title}
+          </p>
+        </div>
+      </div>
       </Link>
     </div>
   );
 };
 
 export default CategoriesCard;
+
